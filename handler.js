e3d91f27c4ce0fe97ef4cb1c2b2dc47aeb0d1411
James Wade <jpswade@gmail.com>
2017-10-15 23:27:57 +0100

    Correction

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ const check = require('./check.js');
 // @see https://github.com/serverless/examples/tree/master/aws-node-serve-dynamic-html-via-http-endpoint
 module.exports.landingPage = (event, context, callback) => {
   // @see https://serverless.com/framework/docs/providers/aws/events/apigateway/
-  const html = check(context.identity.userAgent);
+  const html = check(event.requestContext.identity.userAgent);
   const response = {
     statusCode: 200,
     headers: {
